6d54452f216225d9bd35db0dc410229baa639911
Artem Kashyrin <Gabatawr.dev@gmail.com>
2023-02-27 21:14:25 +0200

    App003 : fix

--- App003.Protobuf/Proto/.ps1
+++ App003.Protobuf/Proto/.ps1
@@ -1,5 +1,7 @@
 ﻿param ($soldir, $prjdir)
 
+Remove-Item -Path ./Generated -Filter *.cs -Recurse
+
 Get-ChildItem -Path ./ -Filter *.proto -Recurse | ForEach-Object {
     protoc --proto_path=$soldir --csharp_out=$prjdir\Proto\Generated\ $_.FullName
 }
