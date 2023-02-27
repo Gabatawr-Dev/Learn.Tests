4f80b600937f93f9e65d0e6788602d27d0abd230
Artem Kashyrin <Gabatawr.dev@gmail.com>
2023-02-27 22:27:49 +0200

    App003 : fix

--- App003.Protobuf/Program.cs
+++ App003.Protobuf/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace App003.Protobuf;
+﻿using App003.Protobuf.Proto.Generated;
+
+namespace App003.Protobuf;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var d = new Data
+        {
+            SomeString = "Hello World!",
+            SomeInt = 42,
+        };
+        Console.WriteLine(d.SomeString);
+        Console.WriteLine(d.SomeInt);
     }
 }
--- App003.Protobuf/Proto/.ps1
+++
@@ -1,7 +0,0 @@
-﻿param ($soldir, $prjdir)
-
-Remove-Item -Path ./Generated -Filter *.cs -Recurse
-
-Get-ChildItem -Path ./ -Filter *.proto -Recurse | ForEach-Object {
-    protoc --proto_path=$soldir --csharp_out=$prjdir\Proto\Generated\ $_.FullName
-}
